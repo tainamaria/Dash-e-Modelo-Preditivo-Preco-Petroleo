86c72012a13bb25d91c6286dfdbf367f5f334d54
kfpetruz <keilamker@gmail.com>
2024-01-21 22:53:50 -0300

    Atualização dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -19,6 +19,9 @@ dados.Preco = dados.Preco/100
 dados = dados.set_index('Data')
 
 ## TABELAS
+menor_data = dados.index.min()
+link_image = 'https://drive.google.com/drive/folders/10zGehcEkLUkTncPGBuabC-OMwCE70pvM?usp=sharing'
+
 media_mensal_anos = dados.groupby(pd.Grouper(freq= 'M'))[['Preco']].mean().reset_index() #Agrupando os dados por mês
 media_mensal_anos['Ano'] = media_mensal_anos['Data'].dt.year.astype(int)
 media_mensal_anos['Mes'] = media_mensal_anos['Data'].dt.month_name()
@@ -62,29 +65,47 @@ fig_vales_preco.update_xaxes(type='category')
 fig_vales_preco.update_layout(xaxis_title= 'Data', yaxis_title = 'Preço')
 
 
-
-
 ## VISUALIZAÇÃO NO STREAMLIT
+### Cartões
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    st.metric('Dados atualizados até:', dados.index.max().strftime('%d/%m/%Y')) 
+with col2:
+    st.metric('Dados monitorados desde:', dados.index.min().strftime('%d/%m/%Y')) 
+with col3:
+    st.metric('Menor preço histórico:', dados['Preco'].min())
+with col4:
+    st.metric('Menor preço histórico:', dados['Preco'].max())
+
+### Gráficos
 col1, col2 = st.columns(2)
 with col1:
-    st.plotly_chart(fig_linha_data)
-    st.plotly_chart(fig_picos_preco)
+    st.plotly_chart(fig_linha_data, use_container_width=True)
+    st.plotly_chart(fig_picos_preco, use_container_width=True)
 with col2:
-    st.plotly_chart(fig_media_mensal_anos)
-    st.plotly_chart(fig_vales_preco)    
-    
+    st.plotly_chart(fig_media_mensal_anos, use_container_width=True)
+    st.plotly_chart(fig_vales_preco, use_container_width=True)    
+
+### Insights
+st.write('## Insights')
+st.image('img/img_pontos_historicos.JPG', caption='Histórico de preços')
+
+st.markdown('<h3 style="color:#ED7D31; text-align: left;"> Preços mais altos históricos </h3>', unsafe_allow_html = True)
+st.markdown('<h3 style="color:#70AD47; text-align: left;"> Preços mais baixos históricos </h3>', unsafe_allow_html = True)
 
 st.dataframe(dados.head(20))
+st.metric('linhas dados',dados.shape[0])
 
 st.dataframe(media_mensal_anos.head(20))
+st.metric('linhas media_mensal',media_mensal_anos.shape[0])
 
 st.dataframe(picos_preco)
 #gráfico comparando os preços médios por mês e ano (semelhante ao do projeto da Alura) - OK
 #gráfico mostrando as datas que houve pico de preço e o preço - OK
 #gráfico mostrando as datas que houve preço mais baixo e o preço - OK
 #Cartões com:
-#Maior preço histórico, menor preço, desde quando é medido
-#publicar 
+#Maior preço histórico, menor preço, desde quando é medido - OK
+#publicar - 
 #dar uma olhada nos filtros que a Tainá tá fazendo pra ver se acho o pq não tá funcionando
 #Alimentar pág Desenvolvimento
 
