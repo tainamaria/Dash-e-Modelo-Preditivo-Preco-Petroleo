8693c8198c52cfa61cf00900e44e2442cbd443ed
kfpetruz <keilamker@gmail.com>
2024-01-22 22:35:37 -0300

    Atualização de referências e dashboard

--- pages/Informações do Projeto.py
+++ pages/Informações do Projeto.py
@@ -3,6 +3,12 @@ import streamlit as st
 st.title('Desenvolvimento do Projeto')
 
 
+
+
 st.markdown('Referências')
 st.markdown('[Fonte de Dados](http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view)')
-st.markdown('[Jornal Nexo - março/2022](https://www.nexojornal.com.br/expresso/2022/03/17/5-gr%C3%A1ficos-para-entender-20-anos-de-pre%C3%A7os-da-gasolina)')
+st.markdown('[Repositório do projeto no Github](https://github.com/kfpetruz/Dash-e-Modelo-Preditivo-Combustiveis)') # Lembrar de tornar público para entregar
+st.markdown('[Site Exame - publicado em novembro/2015](https://exame.com/economia/precos-do-petroleo-se-aproximam-do-fundo-do-poco-de-2008/)')  
+st.markdown('[Jornal Nexo - março/2022](https://www.nexojornal.com.br/expresso/2022/03/17/5-gr%C3%A1ficos-para-entender-20-anos-de-pre%C3%A7os-da-gasolina)')  
+
+
