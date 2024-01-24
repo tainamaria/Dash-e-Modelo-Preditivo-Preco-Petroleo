35cb1501ccfb8a0b3c42ff7090fb1c10dc01ea4d
kfpetruz <keilamker@gmail.com>
2024-01-23 21:59:14 -0300

    Alterações dash e pages

---
+++ pages/Dashboard - Insights.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+### Insights
+st.write('## Insights')
+st.image('img/img_pontos_historicos.JPG', caption='Histórico de preços')
+
+#Altas
+st.markdown('<h3> Principais fatos históricos que afetaram os preços </h3>', unsafe_allow_html = True)
+
+st.markdown('<p style="text-align: justify;"><span style="color:#ED7D31; font-weight: bold">Julho/2008:</span> o fato que culmina no pico de preço do barril de petróleo nesse período, vem acompanhado de uma curva crescente desde o ano de 2004, com instabilidade geopolítica, crescimento da demanda global, especulação no mercado financeiro. Em 2005, por exemplo, o evento climático do furacão Katrina causou danos significativos às instalações de produção de petróleo e gás no Golfo do México, afetando a oferta. Tensões geopolíticas em regiões chave de produção de petróleo, como o Oriente Médio, também contribuíram para a preocupação com a segurança no fornecimento. Eventos como a tensão entre os Estados Unidos e o Irã, assim como conflitos em regiões produtoras, geraram incertezas que influenciaram nos crescentes preços do petróleo.</p>', unsafe_allow_html = True)
+
+st.markdown('<p style="text-align: justify;"> <span style="color:#70AD47; font-weight: bold">Janeiro/2009:</span> a recessão causada pela crise financeira global ocorrida em 2008 e conhecida como "subprime" foi a grande causadora da queda abrupta do preço do petróleo nesse período. A crise, ocasionada pelo estouro da bolha imobiliária nos Estados Unidos, levou principalmente os próprios EUA e a Europa a uma profunda recessão derrubando a demanda por petróleo e, consequentemente, seu preço.</p>', unsafe_allow_html = True)
+
+st.markdown('<p style="text-align: justify;"> <span style="color:#ED7D31; font-weight: bold">Maio/2011:</span>  Após a crise do subprime em 2008, diversas economias globais começaram a se recuperar, resultando em um aumento na demanda por energia, especialmente nos países em desenvolvimento. Além disso, eventos geopolíticos, como instabilidades no Oriente Médio e conflitos em importantes regiões produtoras de petróleo, geraram preocupações sobre a segurança no fornecimento, impactando os preços do petróleo. A desvalorização contínua do dólar americano também desempenhou um papel, tornando o petróleo mais caro para compradores que utilizavam outras moedas. Essa combinação de fatores contribuiu para a retomada da trajetória de alta nos preços dos combustíveis durante o período mencionado, culminando em maio de 2011 </p>', unsafe_allow_html = True) 
+
+#TIRAR MARÇO DE 2012 NA IMAGEM
+
+st.markdown('<p style="text-align: justify;"> <span style="color:#70AD47; font-weight: bold">Janeiro/2016:</span>  xpto xptoxpto xptoxpto xptoxpto xptoxpto xptoxpto xpto </p>', unsafe_allow_html = True)
+st.markdown('<p style="text-align: justify;"> <span style="color:#70AD47; font-weight: bold">Abril/2020:</span>  xpto xptoxpto xptoxpto xptoxpto xptoxpto xptoxpto xpto </p>', unsafe_allow_html = True)
+st.markdown('<p style="text-align: justify;"> <span style="color:#ED7D31; font-weight: bold">Março/2022:</span>  xpto xptoxpto xptoxpto xptoxpto xptoxpto xptoxpto xpto </p>', unsafe_allow_html = True) 
+st.markdown('<p style="text-align: justify;"> <span style="color:#ED7D31; font-weight: bold">Junho/2022:</span>  xpto xptoxpto xptoxpto xptoxpto xptoxpto xptoxpto xpto </p>', unsafe_allow_html = True)  
+
+#AVALIAR COLOCAR O GRÁFICO COM O DÓLAR COMERCIAL HISTÓRICO JA QUE ELE E UM DOS PRINCIPAIS FATORES DA VRIAÇÃO DE PRECOS
--- pages/Sobre o Projeto.py
+++ pages/Sobre o Projeto.py
@@ -6,7 +6,8 @@ st.title('Desenvolvimento do Projeto 🛠️')
 
 
 st.markdown('Referências')
-st.markdown('[Fonte de Dados](http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view)')
+st.markdown('[Fonte de dados do Preço do Petróleo](http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view)')
+st.markdown('[Fonte de dados da taxa de Câmbio Dólar (US$) - Real (R$) ](http://www.ipeadata.gov.br/ExibeSerie.aspx?serid=38590&module=M)')
 st.markdown('[Repositório do projeto no Github](https://github.com/kfpetruz/Dash-e-Modelo-Preditivo-Combustiveis)') # Lembrar de tornar público para entregar
 st.markdown('[Site Exame - publicado em novembro/2015](https://exame.com/economia/precos-do-petroleo-se-aproximam-do-fundo-do-poco-de-2008/)')  
 st.markdown('[Jornal Nexo - março/2022](https://www.nexojornal.com.br/expresso/2022/03/17/5-gr%C3%A1ficos-para-entender-20-anos-de-pre%C3%A7os-da-gasolina)')  
