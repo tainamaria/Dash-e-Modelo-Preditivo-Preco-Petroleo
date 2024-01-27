8ea55e8451e01ebdc2da7be22d158cbffdcaadd6
Taina Maria <tmdpaula@rd.com.br>
2024-01-27 11:56:47 -0300

    Rever biblioteca statsmodels

--- utils.py
+++ utils.py
@@ -95,14 +95,6 @@ def teste_estatistico(dados,string_teste):
     #     st.plotly_chart(fig)
     #     # st.pyplot(fig)
 
-    plot_acf(dados, lags=30)
-
-    # # Salvar a figura como um arquivo de imagem
-    # fig.savefig('acf_plot.png')
-
-    # # Exibir a imagem no Streamlit
-    # st.image('acf_plot.png')
-
     # col1, col2 = st.columns(2)
     # with col1:
     #     fig, ax = plt.subplots()
