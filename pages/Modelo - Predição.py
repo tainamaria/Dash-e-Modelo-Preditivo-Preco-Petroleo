09ab63cd46bc72ac49c6d8ce1aca7fb8c7e07449
Taina Maria <tmdpaula@rd.com.br>
2024-01-28 23:17:39 -0300

    Alteração da função que chama o modelo

--- pages/Modelo - Predição.py
+++ pages/Modelo - Predição.py
@@ -116,7 +116,7 @@ with col4:
     opcao_sazonalidade = st.selectbox("Sazonalidade:", ['add', 'additive', 'mul', 'multiplicative'], index = indice_sazonalidade)
 
 # Carregar a função do modelo de previsão
-forecasting = modelo_ets_previsao(dados, qt_dias_historicos, qt_dias_prever, opcao_tendencia, opcao_sazonalidade)
+forecasting = modelo_carregado(dados, qt_dias_historicos, qt_dias_prever, opcao_tendencia, opcao_sazonalidade)
 
 # Criação de um data frame para juntar os dados previstos e os dias futuros
 df_forecasting = pd.DataFrame()
